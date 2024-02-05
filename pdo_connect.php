748c41f8ef5ef0fe6c6f29c954fed2d17b90dded
LauranceLi <138154118+LauranceLi@users.noreply.github.com>
2024-02-05 15:29:08 +0800

    新增 pdo_connect （資料庫連結方法）

---
+++ pdo_connect.php
@@ -0,0 +1,24 @@
+<?php
+
+$db_host = "172.23.52.126";
+$db_user = 'thirdgroup';
+$db_pass = '030303'; 
+$db_name = 'the_travel_project'; 
+
+$dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4";
+
+$pdo_options = [
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+]; # 可視性佳
+
+$pdo = new PDO($dsn, $db_user, $db_pass);
+
+$sql = "SELECT * FROM members LIMIT 1";
+$stmt = $pdo->query($sql);
+
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); # PDO::FETCH_ASSOC 可視性佳
+
+header('Content-Type: application/json');
+
+echo json_encode($rows);
