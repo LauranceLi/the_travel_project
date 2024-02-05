66558d891185d04a86e8844912471bfb855bebcb
LauranceLi <138154118+LauranceLi@users.noreply.github.com>
2024-02-05 15:37:55 +0800

    增加提示註釋

--- pdo_connect.php
+++ pdo_connect.php
@@ -14,7 +14,7 @@ $pdo_options = [
 
 $pdo = new PDO($dsn, $db_user, $db_pass);
 
-$sql = "SELECT * FROM members LIMIT 1";
+$sql = "SELECT * FROM members LIMIT 1"; # 根據自己負責的表單修改
 $stmt = $pdo->query($sql);
 
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC); # PDO::FETCH_ASSOC 可視性佳
