f9cda6f9850fa37933ebe27c52d6a6579c0e4fa8
LauranceLi <lars108218@gmail.com>
2024-03-09 01:21:23 +0800

    新增 index、index_

---
+++ index_.php
@@ -0,0 +1,17 @@
+<?php
+  // require __DIR__ ."/parts/pdo-connect.php";
+  $title = "Index";
+  $pageName = 'index';
+?>
+<?php include __DIR__. '/parts/html-head.php' ?>
+<?php include __DIR__. '/parts/spinner.php' ?>
+<?php include __DIR__. '/parts/slidebar.php' ?>
+<?php include __DIR__. '/parts/navbar.php' ?>
+
+
+
+
+
+
+<?php include __DIR__. '/parts/html-foot.php' ?>
+<?php include __DIR__. '/parts/scripts.php' ?>
