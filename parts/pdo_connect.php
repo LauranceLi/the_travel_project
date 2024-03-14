0fc85d133652c68f588b7158b792e76ddd5aaa94
LauranceLi <138154118+LauranceLi@users.noreply.github.com>
2024-03-14 12:51:01 +0800

    新增 pdo使用指南

--- parts/pdo_connect.php
+++ parts/pdo_connect.php
@@ -2,7 +2,7 @@
 
 $db_host = "127.0.0.1";
 $db_user = 'the_travel_project';
-$db_pass = '030303'; # 沒有密碼就留空字串
+$db_pass = ''; # 沒有密碼就留空字串
 $db_name = 'the_travel_project'; # 資料庫名稱未定
 
 $dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4";
@@ -14,14 +14,3 @@ $pdo_options = [
 
 $pdo = new PDO($dsn, $db_user, $db_pass);
 
-// $sql = "SELECT * FROM members LIMIT 1";
-// $stmt = $pdo->query($sql);
-
-// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC); # PDO::FETCH_ASSOC 可視性佳
-
-
-
-
-// header('Content-Type: application/json');
-
-// echo json_encode($rows);
