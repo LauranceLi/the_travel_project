d35c4a5800958c37d8f7029df4219d0adebf2ee6
LauranceLi <138154118+LauranceLi@users.noreply.github.com>
2024-03-18 15:36:00 +0800

    新增員工管理、會員管理頁面

---
+++ members.php
@@ -0,0 +1,18 @@
+<?php
+  require __DIR__ . '/parts/pdo_connect.php';
+  session_start();
+  $title = "會員管理";
+  $pageName = 'members';
+  
+?>
+
+<?php include __DIR__. '/parts/html-head.php' ?>
+<?php include __DIR__. '/parts/spinner.php' ?>
+<?php include __DIR__. '/parts/slidebar.php' ?>
+<?php include __DIR__. '/parts/navbar.php' ?>
+
+
+
+<?php include __DIR__. '/parts/footer.php' ?>
+<?php include __DIR__ . '/parts/scripts.php' ?>
+<?php include __DIR__ . '/parts/html-foot.php' ?>
